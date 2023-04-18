26501ddcd8fb66110b41a3c0f2d683afc1bc12cc
Julia <julia.panfilova@mail.ru>
2023-04-18 22:16:19 +0300

    Tests for 100% coverage added

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,41 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
+    const input = [
         "Гарри Поттер",
         "Властелин Колец",
         "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+      ]
+    const expexted = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ]
+    const result = sorting.sortByName(input)
+
+    expect(result).toEqual(expexted)
   });
-});
+  
+  it("Wrong format data should throw an error", () => {
+    const numbers = [1, 1589, 0];
+    expect(()=> sortByName(numbers)).toThrow(ReferenceError)
+  });
+
+  it("Empty data should throw an error", () => {
+    expect(()=> sortByName()).toThrow(ReferenceError)
+  });
+
+  it("Equal book titles should not be sorted", () => {
+    const input = [
+        "Гарри Поттер",
+        "Гарри Поттер",
+      ]
+    const expexted = [
+        "Гарри Поттер",
+        "Гарри Поттер",
+    ]
+    const result = sorting.sortByName(input)
+
+    expect(result).toEqual(expexted)
+  });
+})
